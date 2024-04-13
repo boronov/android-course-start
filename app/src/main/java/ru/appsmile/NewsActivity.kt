963cb222a33b4c94859873dd10f02a9556de1644
Faridun Boronov <38186062+boronov@users.noreply.github.com>
2024-04-13 20:06:12 +0500

    wip

---
+++ app/src/main/java/ru/appsmile/NewsActivity.kt
@@ -0,0 +1,13 @@
+package ru.appsmile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.appsmile.first.R
+
+class NewsActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
+}
