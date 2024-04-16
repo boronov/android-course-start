63d8d8e52e1b4730cfbbe78c236d3d5c56d10575
Faridun Boronov <38186062+boronov@users.noreply.github.com>
2024-04-16 19:44:03 +0500

    add recycler view

--- app/src/main/java/ru/appsmile/NewsActivity.kt
+++ app/src/main/java/ru/appsmile/NewsActivity.kt
@@ -2,12 +2,22 @@ package ru.appsmile
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import ru.appsmile.adapter.NumberAdapter
 import ru.appsmile.first.R
 
-class NewsActivity: AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
+
+    private var numberPanel: RecyclerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+        numberPanel = findViewById(R.id.recycler_view)
+        numberPanel?.layoutManager =
+            StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
+        numberPanel?.adapter = NumberAdapter((1..100).toList())
     }
 }
---
+++ app/src/main/java/ru/appsmile/adapter/NumberAdapter.kt
@@ -0,0 +1,79 @@
+package ru.appsmile.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import ru.appsmile.first.R
+
+class NumberAdapter(private val numberList: List<Int>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    class NumberViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.text_view)
+    }
+
+    class NumberSecondViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.text_view)
+        val imageView: ImageView = view.findViewById(R.id.image_view)
+    }
+
+    override fun getItemViewType(position: Int): Int = if (numberList[position] % 2 == 0) {
+        RED_VIEW_TYPE
+    } else {
+        BLACK_VIEW_TYPE
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            RED_VIEW_TYPE -> {
+                val view =
+                    LayoutInflater.from(parent.context)
+                        .inflate(R.layout.item_number_list, parent, false)
+
+                NumberViewHolder(view)
+            }
+
+            BLACK_VIEW_TYPE -> {
+                val view =
+                    LayoutInflater.from(parent.context)
+                        .inflate(R.layout.item_number_second_list, parent, false)
+
+                NumberSecondViewHolder(view)
+            }
+
+            else -> throw Exception()
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return numberList.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when {
+            holder is NumberViewHolder -> {
+                holder.textView.text = numberList[position].toString()
+            }
+
+            holder is NumberSecondViewHolder -> {
+                holder.textView.text = numberList[position].toString()
+
+                if (numberList[position] % 5 == 0) {
+                    holder.imageView.setImageResource(R.drawable.bg_first)
+                } else {
+                    holder.imageView.setImageResource(R.drawable.bg_second)
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val RED_VIEW_TYPE = 1
+        const val BLACK_VIEW_TYPE = 2
+    }
+}
